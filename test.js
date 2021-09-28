1b746f43b3757cad8fd4b9f2c536a4060255873a
iuhggiuhhgbnr <80612879+iuhggiuhhgbnr@users.noreply.github.com>
2021-09-28 11:39:17 +0700

    update js

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
-var abc = ["1", "2", "3", "4", "5", "6", "7"];
+var abc = ["1", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ"];
 var defaultValues = ["0", "0", "0", "0", "0", "0", "0"];
-
+var defaultValues2 = [undefined,undefined,undefined,undefined,undefined,undefined,undefined]
+var defaultValues3 = new Array(7);
+var defaultValues4 = [randomIntValue(), undefined, undefined, undefined, undefined, undefined, undefined];
 
 var runtimecall = setInterval(makeChart, 1000);
 function makeChart(){
@@ -10,34 +12,66 @@ function makeChart(){
       
     var makeAChart = document.getElementById('myChart').getContext('2d');
     var barchart = new Chart(makeAChart, {
-        type: "line",
+        type: "line",       
         data: {
           labels: abc,
           datasets: [{
-          //  labels: xValues,
             fill : false,
             lineTension : 0,
             backgroundColor: "rgba(0,0,255,1.0)",
             borderColor: "rgba(0,0,255,0.1)",
-            data: defaultValues
+            data: defaultValues4,
+            steppedLine : true,
+            
           }]
         },
         options: {
+          plugins: {
+            title: {
+              display: true,
+               
+            }
+          },
+          responsive: true,         
           legend: {display: false},
+          interaction: {
+            intersect: false,
+            axis: 'x'
+          },
           scales: {
-            yAxes: [{ticks: {min: 0, max:20}}],
-          }
+            yAxes: [{ticks: {min: 0, max:20,beginAtZero: true}                      
+            }],
+          },        
         }
+          
       });    
     
       var d = new Date();
       var t = d.toLocaleTimeString();
+      console.log(defaultValues4[0]);
 
-      barchart.data.labels.push(t);
-      barchart.data.labels.splice(0, 1); 
+      if(defaultValues4[7] == undefined){
+        barchart.data.labels.unshift(t);  
+        barchart.data.labels.splice(8, 1);
+         barchart.data.labels.sort();
+        barchart.data.datasets[0].data.splice(7, 1);
+        barchart.data.datasets[0].data.unshift(randomIntValue());
+        barchart.update();
+        console.log(defaultValues4);
+      }
+      else{
+        barchart.data.labels.push(t);
+     barchart.data.labels.splice(0, 1); 
       barchart.data.datasets[0].data.splice(0, 1);
       barchart.data.datasets[0].data.push(randomIntValue());
       barchart.update();
+      console.log(barchart.data.labels);
+      }
+      //barchart.data.labels.push(t);
+     // barchart.data.labels.splice(0, 1); 
+      //barchart.data.datasets[0].data.splice(0, 1);
+      //barchart.data.datasets[0].data.push(randomIntValue());
+      //barchart.update();
       //console.log(barchart.data.labels);
 }
 
@@ -67,7 +101,6 @@ function ScreenResolution(){
 var callrandomNumberThing = setInterval(randomNumberThing , 100);
 function randomNumberThing(){
   var testValue = Math.floor((Math.random() * 19) + 1);
-  //console.log(testValue);
   return testValue;
 }
 
