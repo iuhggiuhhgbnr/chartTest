fcf5a8f898f9db9568c04c57a788ac3fbbdedf03
iuhggiuhhgbnr <80612879+iuhggiuhhgbnr@users.noreply.github.com>
2021-09-28 16:35:46 +0700

    update js for button chart function

--- test.js
+++ test.js
@@ -1,31 +1,38 @@
-var abc = ["1", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ"];
-var defaultValues = ["0", "0", "0", "0", "0", "0", "0"];
-var defaultValues2 = [undefined,undefined,undefined,undefined,undefined,undefined,undefined]
-var defaultValues3 = new Array(7);
+var valueAxisX = ["1", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ"];
 var defaultValues4 = [randomIntValue(), undefined, undefined, undefined, undefined, undefined, undefined];
 
+var typeofChart = "line";
+var isChartStep = true;
+
+var totalDuration = 10000;
+var delayBetweenPoints = totalDuration / defaultValues4.length;
+
+let a =1;
+
 var runtimecall = setInterval(makeChart, 1000);
-function makeChart(){
-  //var xValues = ["6.00","8.00","9.00","11.00","13.00","15.00","17.00","19.00","21.00","23.00"];
-  //var yValues = [randomNumberThing(),randomNumberThing(),randomNumberThing(),randomNumberThing(),randomNumberThing(),randomNumberThing(),randomNumberThing(),randomNumberThing(),randomNumberThing(),randomNumberThing()];
-  
-      
+function makeChart(){ 
     var makeAChart = document.getElementById('myChart').getContext('2d');
-    var barchart = new Chart(makeAChart, {
-        type: "line",       
+    globalThis.barchart;
+    barchart = new Chart(makeAChart, {
+        type: typeofChart,       
         data: {
-          labels: abc,
+          labels: valueAxisX,
           datasets: [{
             fill : false,
             lineTension : 0,
             backgroundColor: "rgba(0,0,255,1.0)",
             borderColor: "rgba(0,0,255,0.1)",
             data: defaultValues4,
-            steppedLine : true,
+            steppedLine : isChartStep,
             
           }]
         },
         options: {
+          animation: {
+            type: 'number',
+            easing: 'linear',
+            duration: 0,
+        },  
           plugins: {
             title: {
               display: true,
@@ -35,12 +42,12 @@ function makeChart(){
           responsive: true,         
           legend: {display: false},
           interaction: {
-            intersect: false,
-            axis: 'x'
+          intersect: false,
           },
           scales: {
-            yAxes: [{ticks: {min: 0, max:20,beginAtZero: true}                      
+            yAxes: [{ticks: {min: 0, max:20,beginAtZero: true},                      
             }],
+            
           },        
         }
           
@@ -48,24 +55,24 @@ function makeChart(){
     
       var d = new Date();
       var t = d.toLocaleTimeString();
-      console.log(defaultValues4[0]);
+      //console.log(defaultValues4[0]);
 
       if(defaultValues4[7] == undefined){
         barchart.data.labels.unshift(t);  
         barchart.data.labels.splice(8, 1);
-         barchart.data.labels.sort();
+        barchart.data.labels.sort();
         barchart.data.datasets[0].data.splice(7, 1);
         barchart.data.datasets[0].data.unshift(randomIntValue());
         barchart.update();
-        console.log(defaultValues4);
+        //console.log(defaultValues4);
       }
       else{
         barchart.data.labels.push(t);
-     barchart.data.labels.splice(0, 1); 
-      barchart.data.datasets[0].data.splice(0, 1);
-      barchart.data.datasets[0].data.push(randomIntValue());
-      barchart.update();
-      console.log(barchart.data.labels);
+        barchart.data.labels.splice(0, 1); 
+        barchart.data.datasets[0].data.splice(0, 1);
+        barchart.data.datasets[0].data.push(randomIntValue());
+        barchart.update();
+        //console.log(barchart.data.labels);
       }
       //barchart.data.labels.push(t);
      // barchart.data.labels.splice(0, 1); 
@@ -75,12 +82,38 @@ function makeChart(){
       //console.log(barchart.data.labels);
 }
 
+function changechartType(){
+  //let a = 0;
+  if(a == 1){
+    typeofChart = "bar";
+    isChartStep = false;
+    //barchart.update();
+  }
+  else if(a == 2){
+    typeofChart = "line"
+    isChartStep = false;
+   // barchart.update();
+  } 
+  else if(a == 3){
+    typeofChart = "line"
+    isChartStep = true;
+    //barchart.update();
+  }
+  else if(a == 4){
+    typeofChart = "bar";
+    isChartStep = false;
+    a = 1;
+  }
+
+  a++
+  console.log(a);
+}
+
 function randomIntValue(){
   var valueRandom = Math.floor((Math.random() * 20) + 1);
   return valueRandom
 }
 
-//
 var callDataTime = setInterval(DataTime , 1000);
 function DataTime(){
   var d = new Date();
@@ -103,5 +136,13 @@ function randomNumberThing(){
   var testValue = Math.floor((Math.random() * 19) + 1);
   return testValue;
 }
+
+function buttonFunction(){
+  var changeChartType = document.getElementById('button_chart');
+  changeChartType.onclick = changechartType;
+
+}
+
+window.onload = buttonFunction;
 
 
