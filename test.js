6a53d12106fd97aa13cfa33075d9ecb10582caec
iuhggiuhhgbnr <80612879+iuhggiuhhgbnr@users.noreply.github.com>
2021-10-01 16:40:34 +0700

    Update js
    Fix chart not showing correct data

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
-var valueAxisX = ["1", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ"];
-var defaultValues4 = [randomIntValue(), undefined, undefined, undefined, undefined, undefined, undefined];
+var d = new Date();
+var t = d.toLocaleTimeString();
+
+var valueAxisX = ["0", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ", "ᅠᅠ"];
+//var valueY = [randomIntValue(), undefined, undefined, undefined, undefined, undefined, undefined];
+var valueY = [0, undefined, undefined, undefined, undefined,undefined,undefined];
 
 var typeofChart = "line";
 var isChartStep = true;
 
 var totalDuration = 10000;
-var delayBetweenPoints = totalDuration / defaultValues4.length;
+var delayBetweenPoints = totalDuration / valueY.length;
 
 let a =1;
 
@@ -22,7 +26,7 @@ function makeChart(){
             lineTension : 0,
             backgroundColor: "rgba(0,0,255,1.0)",
             borderColor: "rgba(0,0,255,0.1)",
-            data: defaultValues4,
+            data: valueY,
             steppedLine : isChartStep,
             
           }]
@@ -53,27 +57,10 @@ function makeChart(){
           
       });    
     
-      var d = new Date();
-      var t = d.toLocaleTimeString();
-      //console.log(defaultValues4[0]);
+      barchart.update();
+      console.log(valueY);
 
-      if(defaultValues4[7] == undefined){
-        barchart.data.labels.unshift(t);  
-        barchart.data.labels.splice(8, 1);
-        barchart.data.labels.sort();
-        barchart.data.datasets[0].data.splice(7, 1);
-        barchart.data.datasets[0].data.unshift(randomIntValue());
-        barchart.update();
-        //console.log(defaultValues4);
-      }
-      else{
-        barchart.data.labels.push(t);
-        barchart.data.labels.splice(0, 1); 
-        barchart.data.datasets[0].data.splice(0, 1);
-        barchart.data.datasets[0].data.push(randomIntValue());
-        barchart.update();
-        //console.log(barchart.data.labels);
-      }
+
       //barchart.data.labels.push(t);
      // barchart.data.labels.splice(0, 1); 
       //barchart.data.datasets[0].data.splice(0, 1);
@@ -83,30 +70,104 @@ function makeChart(){
 }
 
 function changechartType(){
+  
   //let a = 0;
   if(a == 1){
     typeofChart = "bar";
     isChartStep = false;
-    //barchart.update();
+
   }
   else if(a == 2){
     typeofChart = "line"
     isChartStep = false;
-   // barchart.update();
+ 
   } 
   else if(a == 3){
     typeofChart = "line"
     isChartStep = true;
-    //barchart.update();
+
   }
   else if(a == 4){
     typeofChart = "bar";
     isChartStep = false;
+
     a = 1;
   }
 
   a++
+  console.log(typeofChart);
   console.log(a);
+}
+
+var callagain = setInterval(addDataTest,1000)
+function addDataTest(){
+  var d = new Date();
+  var t = d.toLocaleTimeString();
+  if(valueY[6] == undefined){
+    barchart.data.labels.unshift(t);  
+    barchart.data.labels.splice(7, 1);
+    barchart.data.labels.sort();
+
+    if(valueY[1] ==undefined)
+    {
+    barchart.data.datasets[0].data.splice(6, 1);
+    valueY[1] = randomIntValue();
+  
+    barchart.update();
+    //console.log(valueY);
+  }
+  else if(valueY[2] ==undefined)
+    {
+    barchart.data.datasets[0].data.splice(6, 1);
+    valueY[2] = randomIntValue();
+ 
+    barchart.update();
+    //console.log(valueY);
+  }
+  else if(valueY[3] ==undefined)
+    {
+    barchart.data.datasets[0].data.splice(6, 1);
+    valueY[3] = randomIntValue();
+ 
+    barchart.update();
+    //console.log(valueY);
+  }
+  else if(valueY[4] ==undefined)
+    {
+    barchart.data.datasets[0].data.splice(6, 1);
+    valueY[4] = randomIntValue();
+ 
+    barchart.update();
+    //console.log(valueY);
+  }
+  else if(valueY[5] ==undefined)
+    {
+    barchart.data.datasets[0].data.splice(6, 1);
+    valueY[5] = randomIntValue();
+ 
+    barchart.update();
+    //console.log(valueY);
+  }
+  else if(valueY[6] ==undefined)
+    {
+    barchart.data.datasets[0].data.splice(6, 1);
+    valueY[6] = randomIntValue();
+ 
+    barchart.update();
+    //console.log(valueY);
+  }
+
+  }
+  else{
+    barchart.data.labels.push(t);
+    barchart.data.labels.splice(0, 1); 
+    barchart.data.datasets[0].data.splice(0, 1);
+    barchart.data.datasets[0].data.push(randomIntValue());
+    barchart.update();
+    //console.log(valueY);
+    //console.log("switch to else");
+    //console.log(barchart.data.labels);
+  }
 }
 
 function randomIntValue(){
